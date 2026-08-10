f50df268a60f41227a39de0e729dda2b075c9383
CadaKerem <136694434+CadaKerem@users.noreply.github.com>
2026-08-10 21:35:53 +0300

    chore: remove temporary test file

--- test_tefas.py
+++
@@ -1,7 +0,0 @@
-import pytefas, datetime
-crawler = pytefas.Crawler()
-today = datetime.datetime.now().strftime('%Y-%m-%d')
-start = (datetime.datetime.now() - datetime.timedelta(days=5)).strftime('%Y-%m-%d')
-print('fetching...')
-df = crawler.fetch(start=start, end=today)
-print(df.head())
